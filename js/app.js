22a2c2fbd66c0172ffc88dabfd38e18d2d92d24d
abdallahalkhatatbeh <alkhatatbehabd@gmail.com>
2020-04-18 05:14:35 +0300

    lab2

--- js/app.js
+++ js/app.js
@@ -1,48 +1,130 @@
+$(document).ready(function(){
 
-$(document).ready(function() {
-    function Gallery(img) {
-      this.title = img.title;
-      this.image_url = img.image_url;
-      this.description = img.description;
-      this.keyword = img.keyword;
-      this.horns = img.horns;
-  
-  }
-   Gallery.prototype.render = function() {
-          let $imgClone = $("#gallery").clone();
-          $imgClone.find("h2").text(this.title);
-          $imgClone.find("img").attr("src", this.image_url);
-          $imgClone.find("#p1").text(this.description);
-          $imgClone.find("#p2").text(this.keyword);
-          $imgClone.find('#p3').text(this.horns);
-          // $imgClone.removeClass("img-template");
-          $imgClone.removeAttr("id");
-          $imgClone.attr("id", this.title);
-          $("main section:first").append($imgClone);
-};
+  $.ajax('./data/page-1.json',{method:'get',datatype:'json'} ).then(data =>{
+
+    console.log(data);
+
+    function Horns(horn){
+      this.name= horn.title;
+      this.imgPath=horn.image_url;
+      this.info = horn.description;
+      this.keyword =horn.keyword;
+      this.horns = horn.horns;
+      Horns.all.push(this);
+    }
+    Horns.all = [];
+    for (let i = 0; i < data.length; i++) {
+      new Horns(data[i]);
+    }
+
+    console.log(Horns.all);
+    let options = [];
+    for (let i = 0; i < Horns.all.length; i++) {
+      if(options.includes(Horns.all[i].keyword) ){
+        continue;
+      }else {
+        options.push(Horns.all[i].keyword);
+      }
+
+    }
+    console.log(options);
 
 
+    let selectorKeyWordGenerator = ()=>{
+      options.forEach(element => {
 
-
-
-        const readJson = () => {
-          $.ajax("data/page-1.json", { method: "GET", dataType: "JSON" }).then(data => {
-            data.forEach(dogItem => {
-              let img = new Gallery(dogItem);
-              img.render();
-            });
-          });
-        };
-        readJson();
+        let x = $('<option>');
+        x.text(element);
+        $('#keyselectors').append(x);
       });
-    //   $( "#gallery" ).select(function() {
-        
-    //     }
-    //   });
-      // {
-      //     "image_url": "https://images.unsplash.com/photo-1514036783265-fba9577fc473?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-      //     "title": "Happy Jackson's Chameleon",
-      //     "description": "These are really common in Hawaii",
-      //     "keyword": "chameleon",
-      //     "horns": 2
-      //   },
+
+    };
+    selectorKeyWordGenerator();
+    let containter = $('#container');
+
+    let render = () => {
+      Horns.all.forEach(element => {
+        let divE1 = $('<div>');
+        divE1.addClass(element.keyword);
+        containter.append(divE1);
+        let imgE1 = $('<img>');
+        imgE1.attr('src',element.imgPath);
+        divE1.append(imgE1);
+        let pE1 = $('<p>');
+        pE1.text(`${element.name}`);
+        divE1.append(pE1);
+        let pE2 = $('<p>');
+        pE2.text(`${element.info}`);
+        divE1.append(pE2);
+
+      });
+
+
+
+    };
+
+
+    render();
+
+
+
+
+
+    let optionsFilter = ()=>{
+      $('#keyselectors').change(function(){
+        $('section').hide();
+        var selectedValue = this.value;
+        console.log('test',selectedValue);
+
+        render2(selectedValue);
+
+      });
+
+    };
+    let render2 = (slectedOp)=>{
+      console.log(slectedOp);
+
+      for (let i = 0; i < Horns.all.length; i++) {
+
+        if(Horns.all[i].keyword === slectedOp){
+          let y = $('body');
+
+          let x = $('<section>');
+          y.append(x);
+          let divE1 = $('<div>');
+
+          x.append(divE1);
+
+          let imgE1 = $('<img>');
+
+          imgE1.attr('src',Horns.all[i].imgPath);
+
+          divE1.append(imgE1);
+
+          let pE1 = $('<p>');
+
+          pE1.text(`${Horns.all[i].name}`);
+
+          divE1.append(pE1);
+
+          let pE2 = $('<p>');
+          pE2.text(`${Horns.all[i].info}`);
+
+          divE1.append(pE2);
+
+        } else{ console.log('hidden divs');
+        }
+      }
+
+
+
+    };
+    optionsFilter();
+  });
+
+
+
+
+
+
+});
